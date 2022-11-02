fcf6c9cd5e2bc6ed6a391e023e6e643eb9c2f76d
Trey <trey.delcamp@gmail.com>
2022-11-02 10:52:14 -0500

    commit tree 6859b7c1da5f0a0f6e6e1097c3f55a885efbad2b

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,15 @@
+require 'pry'
 #write your code here
+def countdown(n)
+    while n > 0 
+        # binding.pry
+        puts "#{n} SECOND(S)!"
+        n -=1
+    end
+    'HAPPY NEW YEAR!'
+end
 
-def countdown
+
+def countdown_with_sleep(num)
+    sleep(num)
 end
